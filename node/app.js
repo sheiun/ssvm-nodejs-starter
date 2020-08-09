bb06727a3b683277ab1300a4e2e4f352842db320
sheiun <me.sheiun@gmail.com>
2020-08-09 17:41:07 +0800

    Simple calc

--- node/app.js
+++ node/app.js
@@ -1,16 +1,20 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const { evaluate } = require("../pkg/ssvm_nodejs_starter_lib.js");
 
-const http = require('http');
-const url = require('url');
-const hostname = '0.0.0.0';
+const http = require("http");
+const url = require("url");
+const hostname = "0.0.0.0";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
+  const queryObject = url.parse(req.url, true).query;
+  if (!queryObject["x"] || !queryObject["y"] || !queryObject["op"]) {
+    res.end(
+      `Please use command curl http://${hostname}:${port}/?x=1&op=+&y=2 \n`
+    );
   } else {
-    res.end(say(queryObject['name']) + '\n');
+    let ans = evaluate(queryObject["x"], queryObject["y"], queryObject["op"]);
+    console.log(ans);
+    res.end(ans);
   }
 });
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,31 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+pub fn evaluate(s_x: &str, s_y: &str, op: &str) -> String {
+  let x: i32 = s_x.parse().unwrap();
+  let y: i32 = s_y.parse().unwrap();
+  let r: i32 = match op {
+    "+" => add(x, y),
+    "-" => min(x, y),
+    "*" => mul(x, y),
+    "/" => div(x, y),
+    _ => 0
+  };
+  return String::from("Answer is ") + &r.to_string();
+}
+
+pub fn add(x: i32, y: i32) -> i32 {
+  return x + y;
+}
+
+pub fn min(x: i32, y: i32) -> i32 {
+  return x - y;
+}
+
+pub fn mul(x: i32, y: i32) -> i32 {
+  return x * y;
+}
+
+pub fn div(x: i32, y: i32) -> i32 {
+  return x / y;
 }
